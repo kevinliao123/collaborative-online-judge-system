a9856167bf4143a833b6c030b353c759343b8034
kevin <he88@vt.edu>
2018-09-30 21:40:24 -0400

    Add new problem function 	* add nav bar

--- week2/oj-client/src/app/services/data.service.ts
+++ week2/oj-client/src/app/services/data.service.ts
@@ -1,17 +1,24 @@
-import { Injectable } from '@angular/core';
-import{Problem} from "../models/problem.model";
-import{PROBLEMS} from "../mock-problems";
+import {Injectable} from '@angular/core';
+import {Problem} from "../models/problem.model";
+import {PROBLEMS} from "../mock-problems";
 
 @Injectable()
 export class DataService {
+  problems: Problem[] = PROBLEMS;
 
-  constructor() { }
-
-  getProblems(): Problem[]{
-    return PROBLEMS
+  constructor() {
   }
 
-  getProblem(id: number):Problem {
-    return PROBLEMS.find((problem)=> problem.id === id);
+  getProblems(): Problem[] {
+    return this.problems
+  }
+
+  getProblem(id: number): Problem {
+    return this.problems.find((problem) => problem.id === id);
+  }
+
+  addProblem(problem: Problem): void {
+    problem.id = this.problems.length + 1;
+    this.problems.push(problem);
   }
 }
