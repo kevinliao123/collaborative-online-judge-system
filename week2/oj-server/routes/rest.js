78bcede660c11477de557271e016eb70ded7488f
kevin <he88@vt.edu>
2018-10-22 22:34:19 -0400

    Allow refresh on all url

--- week2/oj-server/routes/rest.js
+++ week2/oj-server/routes/rest.js
@@ -18,7 +18,7 @@ router.post("/problems", jsonParser, function (req, res) {
     problemService.addProblem(req.body).then(function(problem) {
         res.json(problem);
     }, function(error) {
-        res.status(400).send("Problem name already exists!");
+        res.status(400).send(error);
     });
 });
 
--- week2/oj-server/server.js
+++ week2/oj-server/server.js
@@ -6,11 +6,15 @@ var mongoose = require("mongoose");
 var path = require("path")
 
 mongoose.connect("mongodb://user:user123@ds121373.mlab.com:21373/coj", {useNewUrlParser: true});
-app.use(express.static(path.join(__dirname,'../public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.use("/", indexRouter);
 app.use("/api/v1", restRouter);
 
+app.use('/', function(req, res){
+    res.sendFile("index.html",{root:path.join(__dirname,'../public/')});
+});
+
 app.listen(3000, function () {
     console.log('App listening on port 3000!')
-})
+});
